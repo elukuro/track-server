85343c6112d765a125696a6c104ed094993b0666
elukuro <heruhartantot@gmail.com>
2022-01-17 07:10:23 +0700

    Initialize mongoDB

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const app = new express();
+
+const mongoUri = process.env.URL;
+mongoose.connect(mongoUri);
+
+mongoose.connection.on("connected", () => {
+  console.log("connected to mongo instance");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("Error connecting to mongo", err);
+});
 
 app.get("/", (req, res) => {
   res.send("Hi there");
