4e82e6c81931478165e46a0e6de07772429abb4e
elukuro <heruhartantot@gmail.com>
2022-01-19 21:02:25 +0700

    set JWT

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 const User = mongoose.model("User");
 const router = express.Router();
+
+const dotenv = require("dotenv");
+dotenv.config();
 
 router.post("/signup", async (req, res) => {
   const { email, password } = req.body;
   const user = new User({ email, password });
   try {
     const response = await user.save();
+    const token = jwt.sign({ userId: user._id }, process.env.SECRET);
     if (response) {
-      res.json(response);
+      res.json({ token });
     }
   } catch (err) {
     res.status(422).send(err.message);
