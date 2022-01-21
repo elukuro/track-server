271d55205356cf707fe4f6151c961449a00af6ed
elukuro <heruhartantot@gmail.com>
2022-01-21 20:57:27 +0700

    create track route

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -13,4 +13,20 @@ router.get("/tracks", async (req, res) => {
   res.send(tracks);
 });
 
+router.post("/tracks", async (req, res) => {
+  const { name, locations } = req.body;
+  if (!name || !locations) {
+    return res
+      .status(422)
+      .send({ error: "you must provide name and location" });
+  }
+  try {
+    const track = new Track({ name, locations, userId: req.user._id });
+    const response = await track.save();
+    return res.send(response);
+  } catch (err) {
+    res.status(422).send({ error: err.message });
+  }
+});
+
 module.exports = router;
